2a5954d43166172543ef90655f45916973e95442
Arthur Jones <arthur.l.jones@gmail.com>
2016-03-25 00:51:04 -0700

    Add error checking if realpath is missing

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -3,6 +3,11 @@
 script_path=$(realpath $BASH_SOURCE)
 dotfile_dir=${script_path%/*}
 
+if [ -z $dotfile_dir ]; then
+    echo "Failed to determine where dotfiles are stored"
+    exit 1
+fi
+
 for dotfile in bashrc bash_profile profile vimrc; do
     src="$HOME/.$dotfile"
     dst="$dotfile_dir/$dotfile"
