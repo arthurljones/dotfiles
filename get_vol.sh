fe5f8527a6267f03d42532d7f0238935f3f8e8f0
Arthur L Jones <arthur.l.jones@gmail.com>
2016-05-04 11:07:55 -0700

    Output 'N/A' if we can't get the volume

--- get_vol.sh
+++ get_vol.sh
@@ -1,4 +1,12 @@
-amixer \
+#!/bin/bash
+
+output=$(amixer 2>&1)
+if [[ $? -ne 0 ]]; then
+    echo "N/A"
+    exit
+fi
+
+echo $output \
     | grep -e '^\s\+Front Left:' \
     | sed 's/.*\[\([-0-9.]\+\)dB\].*/\1/' \
-    | awk '{printf("%2.0f%\n", (37 + $1) * (100 / 37))}'
+    | awk '{printf("%2.0f%\n", (37 + $1) * (100 / 37))}')
