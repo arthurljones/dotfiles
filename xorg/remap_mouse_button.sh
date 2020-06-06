1154c5537ff169661cefda37be495b32ebb20ee0
Arthur L Jones <arthur.l.jones@gmail.com>
2020-06-06 13:58:39 -0700

    Add mouse button remap udev rule

--- xorg/remap_mouse_button.sh
+++ xorg/remap_mouse_button.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
-echo "$0 `date`" >> $HOME/dotfiles/xorg/log.txt
+{
+    echo "$0 `date`"
 
-if ! type xinput >/dev/null 2>&1; then
-    echo "No xinput on this system, can't remap mouse" >&2
-    exit 1
-fi
+    if ! type xinput >/dev/null 2>&1; then
+        echo "No xinput on this system, can't remap mouse" >&2
+        exit 1
+    fi
 
-if [[ -z "$DISPLAY" ]]; then
-    echo "No \$DISPLAY set" >&2
-    exit 1
-fi
+    if [[ -z "$DISPLAY" ]]; then
+        export DISPLAY=:0
+        echo "No \$DISPLAY set, using $DISPLAY" >&2
+    fi
 
-if ! xset q &>/dev/null; then
-    echo "No X server at \$DISPLAY [$DISPLAY]" >&2
-    exit 1
-fi
+    if [[ -z "$XAUTHORITY" ]]; then
+        export XAUTHORITY=/home/aj/.Xauthority
+        echo "No \$XAUTHORITY set, using $XAUTHORITY" >&2
+    fi
 
-mouse_id=$(xinput list | grep "Logitech.*pointer" | head -n 1 |  sed 's/.*id=\([0-9]\+\).*/\1/g')
+    if ! xset q &>/dev/null; then
+        echo "No X server at \$DISPLAY [$DISPLAY]" >&2
+        exit 1
+    fi
 
-if [ -z "$mouse_id" ]; then
-    echo "Couldn't find mouse ID" >&2
-    exit 1
-fi
+    mouse_id=$(xinput list | grep "Logitech.*pointer" | head -n 1 |  sed 's/.*id=\([0-9]\+\).*/\1/g')
 
-# Map buttons 8 and 9 (top side buttons) to button 2 (middle button)
-xinput set-button-map $mouse_id 1 2 3 4 5 6 7 2 2 10 11 12 13 14 15 16
+    if [ -z "$mouse_id" ]; then
+        echo "Couldn't find mouse ID" >&2
+        exit 1
+    fi
+
+    # Map buttons 8 and 9 (top side buttons) to button 2 (middle button)
+    xinput set-button-map $mouse_id 1 2 3 4 5 6 7 2 2 10 11 12 13 14 15 16
+
+} >> /home/aj/dotfiles/xorg/log.txt 2>&1
