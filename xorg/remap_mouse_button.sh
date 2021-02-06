52801fecdfdcfb82350b73008c912ed1366c590d
Arthur L Jones <arthur.l.jones@gmail.com>
2021-02-05 19:43:33 -0800

    Setup Logitech ERGO M575

--- xorg/remap_mouse_button.sh
+++ xorg/remap_mouse_button.sh
@@ -22,7 +22,8 @@
         exit 1
     fi
 
-    mouse_id=$(xinput list | grep "Logitech.*pointer" | head -n 1 |  sed 's/.*id=\([0-9]\+\).*/\1/g')
+    #mouse_id=$(xinput list | grep "Logitech.*pointer" | head -n 1 |  sed 's/.*id=\([0-9]\+\).*/\1/g')
+    mouse_id=$(xinput list | grep "M575" | head -n 1 |  sed 's/.*id=\([0-9]\+\).*/\1/g')
 
     if [ -z "$mouse_id" ]; then
         echo "Couldn't find mouse ID" >&2
@@ -31,5 +32,7 @@
 
     # Map buttons 8 and 9 (top side buttons) to button 2 (middle button)
     xinput set-button-map $mouse_id 1 2 3 4 5 6 7 2 2 10 11 12 13 14 15 16
+    xinput --set-prop $mouse_id 'libinput Accel Speed' 0
+    xinput --set-prop $mouse_id 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 .4
 
 } >> /home/aj/dotfiles/xorg/log.txt 2>&1
