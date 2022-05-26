4f1b4d5b1a261e6a5419cb0efc01412fcf96fcfe
Arthur L Jones <arthur.l.jones@gmail.com>
2022-05-25 19:50:50 -0700

    Improve input scripts

--- xorg/configure_input.sh
+++ xorg/configure_input.sh
@@ -7,9 +7,12 @@ $xorg_conf/remap_mouse_button.sh
 
 setxkbmap -option ctrl:nocaps -option altwin:swap_alt_win
 
+# Load up our custom xmodmap
+# TODO: configure this so it works with both the built-in and kinesis keyboards
+#xmodmap $xorg_conf/xmodmap
+
 # Set keyboard repeat delay to 400ms and repeat rate to 100
 xset r rate 400 100
 
 # Disable trackpoint
 xinput set-prop "TPPS/2 IBM TrackPoint" "Device Enabled" 0
-
