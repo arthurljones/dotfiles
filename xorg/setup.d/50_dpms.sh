a57d41350f1c3824158e2f2e2597a6c2addf5ba7
Arthur L Jones <arthur.l.jones@gmail.com>
2022-07-01 10:08:27 -0700

    Fix DPMS settings and improve screen sleep

--- xorg/setup.d/50_dpms.sh
+++ xorg/setup.d/50_dpms.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-xset s blank
-xset s 600
+
+xset dpms 600 0 0
+xset s off
+
+## Enable "screensaver"
+#xset s on
+## Stop sending signal (turn screens off)
+#xset s blank
+## Allow blanking even if it causes "expose" events
+#xset s expose
+## Blank screens after X seconds
+#xset s 600
--- xorg/sleep_screen.sh
+++ xorg/sleep_screen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Turning screen(s) off in 1 second"
-sleep 1; xset dpms force off
+sleep 1; xset dpms force standby
