bbc8ae33bfa64fd4a855454f13f23c6b0a6113ca
Arthur L Jones <arthur.l.jones@gmail.com>
2022-07-26 15:53:14 -0700

    Clean up display setup

--- xorg/script-setup.d/40_displays.sh
+++ xorg/script-setup.d/40_displays.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+internal="--output eDP-1 --auto --pos 0x665"
 external=$(xrandr -q | grep " connected" | grep -v "eDP-1" | cut -d ' ' -f1 | head -n1)
-xrandr --output eDP-1 --mode 1920x1080 --pos 0x665 --rotate normal --output HDMI-1 --off --output $external --primary --mode 3440x1440 --pos 1920x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off
+if [ -z $external]; then
+    internal="$internal --primary"
+else
+    external="--output $external --primary --auto --pos 1920x0"
+fi
+xrandr $internal $external
