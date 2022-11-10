047f9bb33a2b474bdc26142a6d8898dae4d7d62d
Arthur L Jones <arthur.l.jones@gmail.com>
2022-11-10 14:08:15 -0800

    Some more hwdb config

--- xorg/script-setup.d/20_input.sh
+++ xorg/script-setup.d/20_input.sh
@@ -4,11 +4,10 @@ xorg_conf=$HOME/dotfiles/xorg
 
 # Setup HW keymappings. Need to do this first, since it
 # causes all inputs to be reset
-pushd $xorg_conf/udev > /dev/null
-sudo ./install_hwdb.sh
-popd > /dev/null
-
-sleep 0.5
+# pushd $xorg_conf/udev > /dev/null
+# sudo ./install_hwdb.sh
+# popd > /dev/null
+# sleep 0.5
 
 # Set keyboard repeat delay to 400ms and repeat rate to 100
 xset r rate 400 100
