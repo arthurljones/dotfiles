ef0ce20d65c1ccb011d46afe6aaf66e7233d3b8a
Arthur L Jones <arthur.l.jones@gmail.com>
2025-02-28 14:54:54 -0800

    Some updates

--- bashrc
+++ bashrc
@@ -72,3 +72,7 @@ fi
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 . "$HOME/.cargo/env"
+
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
+eval "$(/home/aj/.local/bin/mise activate bash)"
--- xorg/script-setup.d/40_displays.sh
+++ xorg/script-setup.d/40_displays.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-internal="--output eDP-1 --auto --pos 0x665"
 ##internal="--output eDP-1 --auto --pos 0x0"
 external=$(xrandr -q | grep " connected" | grep -v "eDP-1" | cut -d ' ' -f1 | head -n1)
 if [ -z $external ]; then
-    internal="$internal --primary"
+    command="xrandr --auto"
 else
+    internal="--output eDP-1 --auto --pos 0x665"
     external="--output $external --primary --auto --pos 1920x0"
+    command="xrandr $internal $external"
 fi
-echo "xrandr $internal $external"
-xrandr $internal $external
+echo "$command"
+`$command`
+xmonad --restart
 # Disabled so the scripts don't require root
 # echo 4 | sudo tee /sys/class/backlight/acpi_video0/brightness
